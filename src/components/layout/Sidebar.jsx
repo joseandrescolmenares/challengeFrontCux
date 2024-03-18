18243a3b5d304ee00736b091ac6ff3228815f635
jose <joseandrescolmenares02@gmail.com>
2024-03-18 15:17:14 -0300

    fix: add several titles to the sidebar

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -1,10 +1,21 @@
-export  const Sidebar = ({setOpenDialog}) => (
-  <sidebar className="w-3/12 border-r-4 b h-full flex col justify-center items-start gap-4 bg-violet-300 p-4" >
-    <div className="border-b-2 border-gray-300 w-full " onClick={() => setOpenDialog(true)}>
-      <p className="text-lg font-semibold p-4 cursor-pointer">Actividades</p>
-    </div>
+import { sidebar } from "../../lib/constants";
+
+export const Sidebar = ({ setOpenDialog }) => (
+  <sidebar className="w-3/12 border-r-4 b h-full flex flex-col justify-start items-start gap-4 bg-violet-300 p-4">
+    {sidebar.map((element) => (
+      <div
+        key={element.title}
+        className="border-b-2 border-gray-300 w-full "
+        onClick={() => {
+          if (element.title === "Actividad") {
+            setOpenDialog(true);
+          }
+        }}
+      >
+        <p className="text-lg font-semibold p-4 cursor-pointer">
+          {element.title}
+        </p>
+      </div>
+    ))}
   </sidebar>
 );
-
-
-
---
+++ src/lib/constants.js
@@ -0,0 +1,6 @@
+export const sidebar = [
+    {title: "Actividad"},
+    {title: "Contame un Secreto"},
+    {title: "Profecionales"},
+
+]
